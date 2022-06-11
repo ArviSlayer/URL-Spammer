c4db5e84f3604d51b1cdfceb6d28ee31985271e5
ArviS <69751083+ArviSlayer@users.noreply.github.com>
2022-06-11 21:09:10 +0300

    Update arvis.js

--- arvis.js
+++ arvis.js
@@ -1,27 +1,24 @@
-const Discord = require("discord.js")
-const client = new Discord.Client() 
-const request = require('request')
-const config = require("./config.json")
-
-
-    client.on("ready", () => {
+client.on("ready", () => {
          const guild = client.guilds.cache.get(config.sunucu)
            console.log(`✅・${guild.name} Sunucusu İçin "${config.vanity}" URL'si Spamlanmaya Başlandı`)
 
 
     setInterval(async () => {
         if(guild.vanityURLCode == config.vanity) {
-            let onay = new Discord.MessageEmbed().setAuthor(guild.name, guild.iconURL({dynamic: true})).setColor('RANDOM').setTimestamp().setDescription(`"**🎉・${guild.name}**" Sunucusunun özel URL'si Belirtildiği Gibi **\`${config.vanity}\`** Olarak Değiştirildi`)
-                let kanal = await guild.channels.cache.get(config.urllog)
-                     await kanal.send(onay)
-                        await kanal.send(`<@${config.botOwner}>`)
+            let onay = new Discord.MessageEmbed()
+            .setAuthor(guild.name, guild.iconURL({dynamic: true}))
+            .setColor('#3bff00')
+            .setTimestamp()
+            .setDescription(`"**🎉・${guild.name}** Sunucusunun Özel URL'si Belirtildiği Gibi **\`${config.vanity}\`** Olarak Değiştirildi`)
+                let kanal = await guild.channels.cache.get(config.urlspammer)
+                     await kanal.send({embeds: [onay]})
 
 
     console.log(`${guild.name} = "${guild.vanityURLCode}" Durum: ✅`) 
         console.log(`❌・Bottan Çıkış Yapıldı`)
             process.exit()
                 } else {
-                    arvis(config.vanity, config.guild, config.token)
+                    arvis(config.vanity, config.guild, process.env.TOKEN)
                          }}, 1*500)})
 
     async function arvis(vanity, token) {
@@ -32,14 +29,13 @@ const config = require("./config.json")
     json: true,
         method: 'PATCH',
             headers: {
-                "Authorization": `Bot ${config.token}`
+                "Authorization": `Bot ${process.env.TOKEN}`
                     }
                         };
 
     request(spammer, (err, res, body) => {
         if (err) {
             return console.log(err)}})}
-
 
 client.login(config.token).catch(err => { console.log("❌・Token Hatalı")})
 
